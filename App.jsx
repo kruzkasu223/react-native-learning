40ee80bbf540ace64ba0e16b4e468cd1af4fb052
Krushang Kasundra <kruzkasu223@gmail.com>
2022-11-19 22:20:51 +0530

    feat(styles): learned styles

--- App.jsx
+++ App.jsx
@@ -1,12 +1,32 @@
-import { View, Text } from "react-native"
+import { View, Text, SafeAreaView, StyleSheet } from "react-native"
 
 const App = () => {
   return (
-    <View>
-      <Text>Helloooooo</Text>
-      <Text>Helloooooo</Text>
-    </View>
+    <SafeAreaView style={styles.SafeArea}>
+      <View style={[styles.container, styles.pink]}>
+        <Text>Helloooooo</Text>
+        <Text style={{ color: "white" }}>Helloooooo</Text>
+        <Text>Helloooooo</Text>
+      </View>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  pink: {
+    backgroundColor: "pink",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    borderColor: "lavendar",
+    borderWidth: 5,
+  },
+  SafeArea: {
+    flex: 2,
+  },
+})
 
 export default App
